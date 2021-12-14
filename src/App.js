df3ed98fc53735084452687a23a55920ee4a7cab
AlexeiLazar87 <AlexeiLazar87@gmail.com>
2021-12-14 17:06:03 +0200

    react3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      demo
-    </div>
-  );
+
+    let [counter, setCounter] = useState(0);
+    const increment = () => {
+        setCounter(++counter)
+    }
+    const decrement = () => {
+        setCounter(--counter)
+    }
+    const increment100 = () => {
+        setCounter(counter + 100)
+    }
+    const decrement100 = () => {
+        setCounter(counter - 100)
+    }
+
+
+    return (
+        <div className="App">
+            <h2>Counter is: {counter}</h2>
+            <br/>
+            <button onClick={increment}>Increment</button>
+            <br/>
+            <button onClick={decrement}>Decrement</button>
+            <br/>
+            <button onClick={increment100}>Increment100</button>
+            <br/>
+            <button onClick={decrement100}>Decrement100</button>
+        </div>
+    );
 }
 
 export default App;
