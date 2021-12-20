a463dd93e7513b609001294de33e58217d6cf548
AlexeiLazar87 <AlexeiLazar87@gmail.com>
2021-12-20 20:58:03 +0200

    react6

--- src/components/post/Post.js
+++
@@ -1,9 +0,0 @@
-export default function Post ({item, find}) {
-    return (
-        <div>
-            <h2>id: {item.id} - title: {item.title}</h2>
-            <div>body: {item.body} (userID: {item.userId})</div>
-            <button onClick={() => find(item.id)}>details</button>
-        </div>
-    )
-}
